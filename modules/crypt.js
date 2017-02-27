bd82905bf26478881a6f03fb877bf7c34b9d8ef6
Alexander Penkin <Sanekpenkin@gmail.com>
2017-02-27 11:13:21 +0300

    Delete crypt.js

--- modules/crypt.js
+++
@@ -1,9 +0,0 @@
-const crypto = require('crypto');
-
-const secret = 'abcdegg';
-module.exports = function crypt(whatToCrypt) {
-    const hash = crypto.createHmac('sha512', secret)
-                   .update(whatToCrypt)
-                   .digest('hex');
-    return (hash);
-};
